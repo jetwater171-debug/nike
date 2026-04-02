c9019cc93c4eb82a6368c7fa08209c2302643c63
jetwater <jetwater171@gmail.com>
2026-04-02 10:56:53 -0300

    Match cart layout to Nike HTML

---
+++ src/app/carrinho/head.tsx
@@ -0,0 +1,26 @@
+export default function Head() {
+  return (
+    <>
+      <link
+        rel="stylesheet"
+        href="https://static.nike.com.br/fonts/HelveticaNeue/fontface-helvetica-neue.style.css"
+      />
+      <link
+        rel="stylesheet"
+        href="https://static.nike.com.br/fonts/Futura/fontface-futura.style.css"
+      />
+      <link
+        rel="stylesheet"
+        href="https://static.nike.com.br/fonts/HelveticaNow/fontface-helvetica-now-display.style.css"
+      />
+      <link
+        rel="stylesheet"
+        href="https://static.nike.com.br/fonts/HelveticaNow/fontface-helvetica-now-text.style.css"
+      />
+      <link
+        rel="stylesheet"
+        href="https://static.nike.com.br/v11-293-0/_next/static/css/2f08778f4c799291.css"
+      />
+    </>
+  );
+}
